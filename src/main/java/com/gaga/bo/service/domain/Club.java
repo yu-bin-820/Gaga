2739bf9c291dc9b27cdd0ff8e0b76ec07dd56c86
YUN <YUN@DESKTOP-3T371AE>
2023-05-17 20:53:03 +0900

    2yun : feat : 도메인 객체 생성
    클럽, 결제 도메인 객체 생성

---
+++ src/main/java/com/gaga/bo/service/domain/Club.java
@@ -0,0 +1,150 @@
+package com.gaga.bo.service.domain;
+
+import java.time.LocalDateTime;
+
+public class Club {
+    private int clubNo;
+    private String clubName;
+    private String clubIntro;
+    private int clubLeaderNo;
+    private int clubMaxMemberNo;
+    private LocalDateTime clubRegDate;
+    private int clubState;
+    private String clubImg;
+    private String clubRegion;
+    private int filterGender;
+    private int filterMinAge;
+    private int filterMaxAge;
+    private String filterTag;
+
+    public Club(int clubNo, String clubName, String clubIntro, int clubLeaderNo, int clubMaxMemberNo, LocalDateTime clubRegDate,
+                int clubState, String clubImg, String clubRegion, int filterGender, int filterMinAge, int filterMaxAge, String filterTag) {
+        this.clubNo = clubNo;
+        this.clubName = clubName;
+        this.clubIntro = clubIntro;
+        this.clubLeaderNo = clubLeaderNo;
+        this.clubMaxMemberNo = clubMaxMemberNo;
+        this.clubRegDate = clubRegDate;
+        this.clubState = clubState;
+        this.clubImg = clubImg;
+        this.clubRegion = clubRegion;
+        this.filterGender = filterGender;
+        this.filterMinAge = filterMinAge;
+        this.filterMaxAge = filterMaxAge;
+        this.filterTag = filterTag;
+    }
+
+    public int getClubNo() {
+        return clubNo;
+    }
+
+    public void setClubNo(int clubNo) {
+        this.clubNo = clubNo;
+    }
+
+    public String getClubName() {
+        return clubName;
+    }
+
+    public void setClubName(String clubName) {
+        this.clubName = clubName;
+    }
+
+    public String getClubIntro() {
+        return clubIntro;
+    }
+
+    public void setClubIntro(String clubIntro) {
+        this.clubIntro = clubIntro;
+    }
+
+    public int getClubLeaderNo() {
+        return clubLeaderNo;
+    }
+
+    public void setClubLeaderNo(int clubLeaderNo) {
+        this.clubLeaderNo = clubLeaderNo;
+    }
+
+    public int getClubMaxMemberNo() {
+        return clubMaxMemberNo;
+    }
+
+    public void setClubMaxMemberNo(int clubMaxMemberNo) {
+        this.clubMaxMemberNo = clubMaxMemberNo;
+    }
+
+    public LocalDateTime getClubRegDate() {
+        return clubRegDate;
+    }
+
+    public void setClubRegDate(LocalDateTime clubRegDate) {
+        this.clubRegDate = clubRegDate;
+    }
+
+    public int getClubState() {
+        return clubState;
+    }
+
+    public void setClubState(int clubState) {
+        this.clubState = clubState;
+    }
+
+    public String getClubImg() {
+        return clubImg;
+    }
+
+    public void setClubImg(String clubImg) {
+        this.clubImg = clubImg;
+    }
+
+    public String getClubRegion() {
+        return clubRegion;
+    }
+
+    public void setClubRegion(String clubRegion) {
+        this.clubRegion = clubRegion;
+    }
+
+    public int getFilterGender() {
+        return filterGender;
+    }
+
+    public void setFilterGender(int filterGender) {
+        this.filterGender = filterGender;
+    }
+
+    public int getFilterMinAge() {
+        return filterMinAge;
+    }
+
+    public void setFilterMinAge(int filterMinAge) {
+        this.filterMinAge = filterMinAge;
+    }
+
+    public int getFilterMaxAge() {
+        return filterMaxAge;
+    }
+
+    public void setFilterMaxAge(int filterMaxAge) {
+        this.filterMaxAge = filterMaxAge;
+    }
+
+    public String getFilterTag() {
+        return filterTag;
+    }
+
+    public void setFilterTag(String filterTag) {
+        this.filterTag = filterTag;
+    }
+
+	@Override
+	public String toString() {
+		return "Club [clubNo=" + clubNo + ", clubName=" + clubName + ", clubIntro=" + clubIntro + ", clubLeaderNo="
+				+ clubLeaderNo + ", clubMaxMemberNo=" + clubMaxMemberNo + ", clubRegDate=" + clubRegDate
+				+ ", clubState=" + clubState + ", clubImg=" + clubImg + ", clubRegion=" + clubRegion + ", filterGender="
+				+ filterGender + ", filterMinAge=" + filterMinAge + ", filterMaxAge=" + filterMaxAge + ", filterTag="
+				+ filterTag + "]";
+	}
+    
+}
