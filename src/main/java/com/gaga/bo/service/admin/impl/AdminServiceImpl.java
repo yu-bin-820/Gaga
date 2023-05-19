55bd976f4ac97a270268b898222ddf399ad9a52c
LEEVATAR <thega4004@naver.com>
2023-05-19 09:51:55 +0900

    4lee : etc : 관리자

--- src/main/java/com/gaga/bo/service/admin/impl/AdminServiceImpl.java
+++ src/main/java/com/gaga/bo/service/admin/impl/AdminServiceImpl.java
@@ -1,5 +1,7 @@
 package com.gaga.bo.service.admin.impl;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
@@ -24,22 +26,32 @@ public class AdminServiceImpl implements AdminService {
 	public void addNoticePost(NoticePost noticePost) {
 		adminDao.addNoticePost(noticePost);
 		System.out.println(noticePost);
-		
+
 	}
-	/*
-	 * @Override public List<NoticePost> listNoticePosts() { return
-	 * noticePostDao.listNoticePost(); }
-	 * 
-	 * @Override public NoticePost getNoticePost(int noticePostNo) { return
-	 * noticePostDao.getNoticePost(noticePostNo); }
-	 * 
-	 * @Override public void updateNoticePost(NoticePost noticePost) {
-	 * noticePostDao.updateNoticePost(noticePost); }
-	 * 
-	 * @Override public void deleteNoticePost(int noticePostNo) {
-	 * noticePostDao.deleteNoticePost(noticePostNo); }
-	 * 
-	 * @Override public List<NoticePost> searchNoticePost(String keyword) { return
-	 * noticePostDao.searchNoticePost(keyword); }
-	 */
+
+	@Override
+	public List<NoticePost> listNoticePost() {
+		return adminDao.listNoticePost();
+	}
+
+	@Override
+	public NoticePost getNoticePost(int noticePostNo) {
+		return adminDao.getNoticePost(noticePostNo);
+	}
+
+	@Override
+	public void updateNoticePost(NoticePost noticePost) {
+		adminDao.updateNoticePost(noticePost);
+	}
+
+	@Override
+	public void deleteNoticePost(int noticePostNo) {
+		adminDao.deleteNoticePost(noticePostNo);
+	}
+
+	//@Override
+	//public List<NoticePost> searchNoticePost(String keyword) {
+	//	return adminDao.searchNoticePost(keyword);
+	//}
+
 }
--- src/test/java/com/gaga/bo/service/admin/test/AdminTest.java
+++ src/test/java/com/gaga/bo/service/admin/test/AdminTest.java
@@ -1,6 +1,12 @@
 package com.gaga.bo.service.admin.test;
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertNull;
+import static org.junit.Assert.assertTrue;
+
 import java.time.LocalDateTime;
+import java.util.List;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -18,19 +24,59 @@ class AdminTest {
 	@Qualifier("adminServiceImpl")
 	AdminService adminService;
 	
-	@Test
+	//@Test
 	public void testAddNoticePost() {
 		NoticePost noticePost = new NoticePost();
 		
 	    noticePost.setNoticePostNo(999);
 	    noticePost.setNoticePostCategoryNo(1);
-	    noticePost.setNoticePostTitle("junit테스트");
+	    noticePost.setNoticePostTitle("junit테스트1");
 	    noticePost.setNoticePostText("junit테스트");
 	    noticePost.setNoticePostImg("asdasdsad.jpg");
 	    noticePost.setNoticePostRegDate(LocalDateTime.now());
 	    noticePost.setUserNo(5);
 	    		
 	    adminService.addNoticePost(noticePost);
+	  
 	}
+	
+	//@Test
+	public void testListNoticePost() {
+		List<NoticePost> noticePost = adminService.listNoticePost();
 
+		// Verify if the list is not empty
+		assertTrue(noticePost.size() > 0);
+	}
+	
+	//@Test
+	public void testGetNoticePost() {
+		// Test the getNoticePost method with an existing post number
+		NoticePost noticePost = adminService.getNoticePost(3);
+
+		// Verify the returned NoticePost
+		//assertNotNull(noticePost);
+		assertEquals(3, noticePost.getNoticePostNo());
+	}
+	//@Test
+	public void testUpdateNoticePost() {
+		NoticePost noticePost = adminService.getNoticePost(3);
+
+		// Update the NoticePost
+		noticePost.setNoticePostTitle("Updated Title업데이트 업업");
+		adminService.updateNoticePost(noticePost);
+
+		// Verify the updated NoticePost
+		NoticePost updatedNoticePost = adminService.getNoticePost(3);
+		assertEquals("Updated Title업데이트 업업", updatedNoticePost.getNoticePostTitle());
+	}
+	//@Test
+	public void testDeleteNoticePost() {
+		// Delete an existing NoticePost
+		adminService.deleteNoticePost(3);
+
+		// Verify if the post was deleted
+		NoticePost deletedNoticePost = adminService.getNoticePost(3);
+		assertNull(deletedNoticePost);
+	}
+	/**/
 }
