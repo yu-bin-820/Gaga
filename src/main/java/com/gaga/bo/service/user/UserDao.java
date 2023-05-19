e958b7145d7d3f53035a701e2a2a8052c0aded60
fra407979 <125714834+fra407979@users.noreply.github.com>
2023-05-19 14:52:32 +0900

    5fe : etc : 유저 도메인
    유저 도메인 수정, userDao, userService, userServiceImpl 작성후
    addUser,updateUser, getUser 3개 추가후
    junit Test 완료

--- src/main/java/com/gaga/bo/service/user/UserDao.java
+++ src/main/java/com/gaga/bo/service/user/UserDao.java
@@ -1,5 +1,18 @@
 package com.gaga.bo.service.user;
 
-public interface UserDao {
+import org.apache.ibatis.annotations.Mapper;
 
+import com.gaga.bo.service.domain.User;
+
+@Mapper
+public interface UserDao {
+	
+	public void addUser(User user) throws Exception;
+	
+	public void updateUser(User user) throws Exception;
+	
+	public User getUser(int userNo) throws Exception;
+	
+//	public void deleteUser(int userNo) throws Exception;
+	
 }
