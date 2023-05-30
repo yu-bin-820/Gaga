b6db73d1f293dfd2d68e094c3ff412fef0ddac37
yyyseul <yunys02@gmail.com>
2023-05-31 08:23:14 +0900

    2yun : feat : 결제, 환불, 계좌인증

--- src/main/java/com/gaga/bo/service/payment/impl/PaymentServiceImpl.java
+++ src/main/java/com/gaga/bo/service/payment/impl/PaymentServiceImpl.java
@@ -1,10 +1,22 @@
 package com.gaga.bo.service.payment.impl;
 
+import java.util.Collections;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 
 import com.gaga.bo.service.domain.Meeting;
 import com.gaga.bo.service.domain.Payment;
@@ -23,6 +35,10 @@ public class PaymentServiceImpl implements PaymentService {
 		
 	}
 	
+	private static final String IMP_KEY = "3120528761584822";
+	private static final String IMP_SECRET = "HeWea1iuXMM1McOJys1PrhUYeMiDc6C3XnEa5h0oIc2bIz3vOo1Kemy2H2xkwmyxSbSz5eZ5l3dNeEUA";
+	private static final String IAMPORT_API_URL = "http://api.iamport.kr";
+
 	//Constructor
 	public PaymentServiceImpl() {
 		System.out.println(this.getClass());
@@ -55,7 +71,7 @@ public class PaymentServiceImpl implements PaymentService {
 	}
 
 	@Override
-	public String getPayNoByUserMeeting(Map<String, Integer> refund) throws Exception {
+	public String getPayNoByUserMeeting(Map<String, Object> refund) throws Exception {
 		
 		System.out.println("모임, 유저번호로 결제번호 출력 서비스");
 		
@@ -97,4 +113,44 @@ public class PaymentServiceImpl implements PaymentService {
 		// TODO Auto-generated method stub 	
 	}
 
+	@Override
+	public String getAccessToken() throws Exception {
+		
+		System.out.println("아임포트 토큰 발급 서비스");
+		
+		
+		String accessToken = "";
+		
+		RestTemplate restTemplate = new RestTemplate();
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_JSON);
+        headers.setAccept(Collections.singletonList(MediaType.APPLICATION_JSON));
+
+        JSONObject tokenRequest = new JSONObject();
+        tokenRequest.put("imp_key", IMP_KEY);
+        tokenRequest.put("imp_secret", IMP_SECRET);
+
+        HttpEntity<String> requestEntity = new HttpEntity<>(tokenRequest.toJSONString(), headers);
+        ResponseEntity<String> responseEntity =
+                restTemplate.exchange(IAMPORT_API_URL + "/users/getToken",
+                        HttpMethod.POST,
+                        requestEntity,
+                        String.class);
+
+        if (responseEntity.getStatusCode() == HttpStatus.OK) {
+            try {
+                JSONObject jsonResponse = (JSONObject) new JSONParser().parse(responseEntity.getBody());
+                JSONObject response = (JSONObject) jsonResponse.get("response");
+                accessToken = (String) response.get("access_token");
+            }  catch (ParseException e) {
+                e.printStackTrace();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+
+        return accessToken;
+    }
+		
 }
