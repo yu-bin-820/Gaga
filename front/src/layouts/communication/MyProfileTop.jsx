fef55007d49b8da8936c22b60693568c88371353
SHINISEONG <hss275989@gmail.com>
2023-05-31 09:49:35 +0900

    3shin : etc : MyProfileTop

--- front/src/layouts/communication/MyProfileTop.jsx
+++ front/src/layouts/communication/MyProfileTop.jsx
@@ -117,9 +117,9 @@ const MyProfileTop = () => {
             </ListItem>
             <Divider />
 
-            <Button component={Link} to="/user/updateuser">
+            {/* <Button component={Link} to="/user/updateuser">
                 개인 정보 수정
-              </Button><br/>
+              </Button><br/> */}
             <ListItem
               sx={{
                 marginTop: '10px',
