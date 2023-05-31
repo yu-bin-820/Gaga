5c68cafaedccecb8e324a213901a2e2dc397ef64
fra407979 <125714834+fra407979@users.noreply.github.com>
2023-06-01 08:30:04 +0900

    5fe 이용약관 추가 및 페이지 다수수정

---
+++ front/src/layouts/user/UserBack.jsx
@@ -0,0 +1,57 @@
+import React from 'react';
+import AppBar from '@mui/material/AppBar';
+import Toolbar from '@mui/material/Toolbar';
+import Container from '@mui/material/Container';
+import ArrowBackIosNewIcon from '@mui/icons-material/ArrowBackIosNew';
+import { Button, IconButton, Typography } from '@mui/material';
+import { Link, Outlet, useNavigate } from 'react-router-dom';
+import { Box } from '@mui/system';
+import PropTypes from 'prop-types';
+
+const CommonTop = ({ pageName, prevPath }) => {
+  const navigate = useNavigate();
+  return (
+    <>
+      <AppBar
+        position="fixed"
+        color="secondary"
+        elevation={0}
+        sx={{ height: '58px', borderBottom: '1px solid #ccc' }}
+      >
+        <Container maxWidth="xl">
+          <Toolbar disableGutters>
+            <IconButton
+              onClick={() => {
+                navigate(prevPath || -1);
+              }}
+            >
+              <ArrowBackIosNewIcon />
+            </IconButton>
+            <Box
+              sx={{
+                minWidth: 'calc(100vw - 108px)',
+                display: 'flex',
+                justifyContent: 'center',
+              }}
+            >
+              <Typography
+                sx={{
+                  fontSize: 20,
+                  fontWeight: 700,
+                }}
+              >
+                {pageName}
+              </Typography>
+            </Box>
+          </Toolbar>
+        </Container>
+      </AppBar>
+      <Outlet />
+    </>
+  );
+};
+CommonTop.propTypes = {
+  pageName: PropTypes.string,
+  prevPath: PropTypes.string,
+};
+export default CommonTop;
