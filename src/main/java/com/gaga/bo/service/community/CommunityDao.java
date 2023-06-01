e797035bf830217244d65af523ff44ac44e0d035
SHINISEONG <hss275989@gmail.com>
2023-06-01 21:23:57 +0900

    3shin : feat : Profile U, DirectMessage CR

--- src/main/java/com/gaga/bo/service/community/CommunityDao.java
+++ src/main/java/com/gaga/bo/service/community/CommunityDao.java
@@ -63,5 +63,7 @@ public interface CommunityDao {
 	///Title Method
 	public void addUserEarnedTitle(Map<String,Integer> userNoMainCategoryNoLeaderState) throws Exception;
 	
+	public String getTitleName(int titleNo) throws Exception;
+	
 	public List<Title> getUserEarnedTitleList(int userNo) throws Exception;
 }
