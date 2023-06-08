b487beceee84806473b9de8a9a31a93314ccedfb
LEEVATAR <thega4004@naver.com>
2023-06-08 20:38:01 +0900

    4lee : update some method

--- front/src/pages/admin/BlackList/GetUser.jsx
+++ front/src/pages/admin/BlackList/GetUser.jsx
@@ -1,8 +1,17 @@
 import React, { useState, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
 import axios from 'axios';
-import { Box, Button, Typography, Grid, Paper, ListItem, ListItemText, List } from '@mui/material';
-
+import { Box, Button, Typography, Grid, Paper, ListItem, ListItemText, List, ListItemIcon, Icon } from '@mui/material';
+import AddCircleIcon from '@mui/icons-material/AddCircle';
+import RemoveCircleIcon from '@mui/icons-material/RemoveCircle';
+import PersonIcon from '@mui/icons-material/Person';
+import CheckCircleIcon from '@mui/icons-material/CheckCircle';
+import BlockIcon from '@mui/icons-material/Block';
+import PhoneIcon from '@mui/icons-material/Phone';
+import AssignmentIndIcon from '@mui/icons-material/AssignmentInd';
+import PermIdentityIcon from '@mui/icons-material/PermIdentity';
+import AccountCircleIcon from '@mui/icons-material/AccountCircle';
+import CommonTop from '@layouts/common/CommonTop';
 
 const GetUser = () => {
   const { userNo } = useParams();
@@ -45,38 +54,59 @@ const GetUser = () => {
   
   return (
     <Box sx={{ margin: '2rem' }}>
-        
-      <Typography variant="h4" gutterBottom>User Details</Typography>
-      <Grid container spacing={2}>
-        <Grid item xs={12} sm={6}>
-          <Paper elevation={3} sx={{ padding: '2rem' }}>
-            <List>
-              <ListItem>
-                <ListItemText primary="User Name" secondary={user.userName} />
-              </ListItem>
-              <ListItem>
-                <ListItemText primary="User NickName" secondary={user.nickName} />
-              </ListItem>
-              <ListItem>
-                <ListItemText primary="User Role" secondary={user.role} />
-              </ListItem>
-              <ListItem>
-                <ListItemText primary="User PhoneNo" secondary={user.phoneNo} />
-              </ListItem>
-              <ListItem>
-                <ListItemText primary="블랙리스트" secondary={user.blacklist} />
-              </ListItem>
-            </List>
-          </Paper>
-        </Grid>
+    <Typography variant="h4" gutterBottom>
+      <CommonTop pageName="회원 상세보기" prevPath="/community/profile/mine" />
+    </Typography>
+    <Grid container spacing={2}>
+      <Grid item xs={12} sm={6}>
+        <Paper elevation={3} sx={{ padding: '2rem' }}>
+          <List>
+            <ListItem>
+              <ListItemIcon>
+                <PersonIcon />
+              </ListItemIcon>
+              <ListItemText primary="User Name" secondary={user.userName} />
+            </ListItem>
+            <ListItem>
+              <ListItemIcon>
+                <PermIdentityIcon />
+              </ListItemIcon>
+              <ListItemText primary="User NickName" secondary={user.nickName} />
+            </ListItem>
+            <ListItem>
+              <ListItemIcon>
+                <AssignmentIndIcon />
+              </ListItemIcon>
+              <ListItemText primary="User Role" secondary={user.role} />
+            </ListItem>
+            <ListItem>
+              <ListItemIcon>
+                <PhoneIcon />
+              </ListItemIcon>
+              <ListItemText primary="User PhoneNo" secondary={user.phoneNo} />
+            </ListItem>
+            <ListItem> 
+              <ListItemIcon>
+                {user.blacklist ? <BlockIcon /> : <CheckCircleIcon />}
+              </ListItemIcon>
+              <ListItemText primary="블랙리스트" secondary={user.blacklist ? 'Yes' : 'No'} />
+            </ListItem>
+          </List>
+        </Paper>
       </Grid>
-      <Box display="flex" justifyContent="center" alignItems="center" padding="2rem 0">
-        <Button variant="contained" color={isBlacklisted ? "secondary" : "primary"} onClick={toggleBlacklist}>
-          {isBlacklisted ? '블랙리스트 해제' : '블랙리스트 추가'}
-        </Button>
-      </Box>
+    </Grid>
+    <Box display="flex" justifyContent="center" alignItems="center" padding="2rem 0">
+      <Button
+        variant="contained"
+        color={isBlacklisted ? 'secondary' : 'primary'}
+        onClick={toggleBlacklist}
+        startIcon={isBlacklisted ? <RemoveCircleIcon /> : <AddCircleIcon />}
+      >
+        {isBlacklisted ? '블랙리스트 해제' : '블랙리스트 추가'}
+      </Button>
     </Box>
-  );
+  </Box>
+);
 };
 
 export default GetUser;
